48d05d87a4e56a449c5187d5dfa06d8c0d60b344
Archie Hilton (thytom) <archie.hilton1@gmail.com>
2021-12-05 21:41:22 +0000

    Use sets in Day 5

--- src/Days/Day5.hs
+++ src/Days/Day5.hs
@@ -2,9 +2,7 @@ module Days.Day5 where
 
 import TestT
 import Data.List.Split (splitOn)
-import Data.Map as DM (elems, Map, empty, filter, size)
-import Data.Map.Strict(insertWith)
-import Data.List(nub)
+import Data.Set (Set, insert, empty, member, size)
 
 tests = [ 
           Test {name="Day 5 Part 1"  , input="day5_test.txt"  , subject=part1, assert=Just "5"}
@@ -19,26 +17,37 @@ run = [
 type Point = (Int, Int)
 type Line = (Point, Point)
 
-type PointMap = Map Point Int
-
 parse :: String -> [Line]
 parse = map (list2tuple . map (list2tuple . map (read :: String -> Int) . splitOn ",") . splitOn " -> ") . lines
         where list2tuple = \[x, y] -> (x, y)
 
 is_perpendicular :: Line -> Bool
-is_perpendicular ((x1, y1), (x2, y2)) = any (==True) [x1 == x2, y1 == y2]
+is_perpendicular p = any (==True) [vertical p, horizontal p]
+
+vertical :: Line -> Bool
+vertical ((x1, _), (x2, _)) = x1 == x2
+
+horizontal :: Line -> Bool
+horizontal ((_, y1), (_, y2)) = y1 == y2
 
 points :: Line -> [Point]
 points p@((x1, y1), (x2, y2)) 
-  | is_perpendicular p = [(x, y) | x <- [min x1 x2..max x1 x2], y <- [min y1 y2..max y1 y2]]
-  | otherwise = [( if x1 > x2 then x1 - n else x1 + n
-                 , if y1 > y2 then y1 - n else y1 + n) | n <- [0..(max x1 x2)-(min x1 x2)]]
+  | vertical p   = [(x1, y) | y<-[min y1 y2..max y1 y2]]
+  | horizontal p = [(x, y1) | x <- [min x1 x2..max x1 x2]]
+  | otherwise = [(x1 + n*xcomp, y1 + n*ycomp)| n <- [0..(max x1 x2)-(min x1 x2)]]
+        where xcomp = (x2 - x1) `div` (abs (x2 -x1))
+              ycomp = (y2 - y1) `div` (abs (y2 - y1))
+
+insert_ :: (Set Point, Set Point) -> Point -> (Set Point, Set Point)
+insert_ (s1, s2) p
+  | member p s1 = (s1, insert p s2)
+  | otherwise = (insert p s1, s2)
 
 intersections :: [Line] -> Int
-intersections = size . DM.filter (>1) . Prelude.foldl (\pm p -> insertWith (+) p 1 pm) DM.empty . concatMap (points)
+intersections = size . snd . foldl (insert_) (empty, empty) . concatMap points
 
 part1 :: String -> String
-part1 = show . intersections . Prelude.filter (is_perpendicular) . parse
+part1 = show . intersections . filter (is_perpendicular) . parse
 
 part2 :: String -> String
 part2 = show . intersections . parse
