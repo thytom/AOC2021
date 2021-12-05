6fd93dcb6cd0f2d981e4afdeecd0d966bd11770e
Archie Hilton (thytom) <archie.hilton1@gmail.com>
2021-12-05 17:48:19 +0000

    Rearrange files

--- src/Main.hs
+++ src/Main.hs
@@ -8,6 +8,7 @@ import Days.Day2 as Day2
 import Days.Day3 as Day3
 import Days.Day4 as Day4
 import Days.Day5 as Day5
+import Days.Day6 as Day6
 
 tmap :: [Test]
 tmap = concat [ Day1.tests
@@ -15,8 +16,9 @@ tmap = concat [ Day1.tests
               , Day3.tests 
               , Day4.tests 
               , Day5.tests 
+              , Day6.tests 
               ]
 
 main :: IO ()
-main = testall Day5.tests
+main = testall Day6.tests
         -- testall tmap
