ae10d94ce1fd2ebca9258cc6e0b93984adfce488
Archie Hilton (thytom) <archie.hilton1@gmail.com>
2021-12-06 00:43:37 +0000

    Hashtables to the rescue. Take off training wheels for Day 5

--- src/Days/Day5.hs
+++ src/Days/Day5.hs
@@ -2,9 +2,13 @@ module Days.Day5 where
 
 import TestT
 import Data.List.Split (splitOn)
-import Data.Set (Set, insert, empty, member, size)
 import Text.Regex
 import Data.Maybe(fromJust)
+
+import Data.HashTable.ST.Basic as H
+
+import Control.Monad.ST
+import Control.Monad
 
 tests = [ 
           Test {name="Day 5 Part 1"  , input="day5_test.txt"  , subject=part1, assert=Just "5"}
@@ -18,6 +22,8 @@ run = [
 
 type Point = [Int]
 type Line = [Int]
+
+type PointTable = H.HashTable Point Int
 
 mtc = mkRegex "([0-9]+),([0-9]+) -> ([0-9]+),([0-9]+)"
 
@@ -41,13 +47,17 @@ points p@[x1, y1, x2, y2]
         where xcomp = (x2 - x1) `div` (abs (x2 - x1))
               ycomp = (y2 - y1) `div` (abs (y2 - y1))
 
-insert_ :: (Set Point, Set Point) -> Point -> (Set Point, Set Point)
-insert_ (s1, s2) p
-  | member p s1 = (s1, insert p s2)
-  | otherwise = (insert p s1, s2)
-
 intersections :: [Line] -> Int
-intersections = size . snd . foldl (insert_) (empty, empty) . concatMap points
+intersections ls = runST $ do ht1 <- newSized (length ps)
+                              ht2 <- newSized (length ps)
+                              Control.Monad.forM_ ps $ \p -> do
+                                      seen <- H.lookup ht1 p
+                                      case seen of
+                                        Just _  -> insert ht2 p 0
+                                        Nothing -> insert ht1 p 0
+                              sz <- size ht2
+                              return sz
+        where ps = concatMap points ls
 
 part1 :: String -> String
 part1 = show . intersections . filter (is_perpendicular) . parse
