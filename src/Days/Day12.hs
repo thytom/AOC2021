7a9f280a5501ab1195e14df995dd0aa937711fd1
Archie Hilton (thytom) <archie.hilton1@gmail.com>
2021-12-12 20:48:36 +0000

    Hashmap speed is insane

--- src/Days/Day12.hs
+++ src/Days/Day12.hs
@@ -9,6 +9,8 @@ import Data.List (intercalate)
 import Data.List (nub)
 import Data.Maybe (fromJust)
 
+import qualified Data.HashMap.Strict as HM
+
 import Control.Monad.State.Lazy
 import Control.Monad
 
@@ -18,15 +20,13 @@ day = D.Day { D.name = "Day 12"
             , D.testinput = T.mkAssertion "day12_test.txt"   "10" "36"
             , D.input     = T.mkAssertion "day12_actual.txt" "3230" "83475"}
 
-data Node k = START | END | Big k | Small k deriving (Eq, Show)
-type Edge = (Node String, Node String)
-type Graph = [(Node String, [Node String])]
-type Path = [Node String]
+type Node = String
+type Graph = HM.HashMap (Node) [Node]
 
 parse :: String -> Graph
-parse input = map (\n -> (n, fromHere n)) nodes
-        where nodes = map (readnode) . nub . splitOn "-" . intercalate "-" . lines $ input
-              edges = filter ((/=START).snd) . concat . map (combs . map (readnode) . splitOn "-") . lines $ input
+parse input = HM.fromList $ map (\n -> (n, fromHere n)) nodes
+        where nodes = nub . splitOn "-" . intercalate "-" . lines $ input
+              edges = filter ((/="start").snd) . concat . map (combs . splitOn "-") . lines $ input
               fromHere n = map (snd) . filter ((==n) . fst) $ edges
 
 combs :: [a] -> [(a, a)]
@@ -34,30 +34,22 @@ combs [] = []
 combs [x] = []
 combs (x:xs) = combs xs ++ concat [[(x, x'), (x', x)] | x' <- xs ]
 
-readnode :: String -> Node String
-readnode "start" = START
-readnode "end" = END
-readnode xs 
-  | all (isUpper) xs = Big xs
-  | all (isLower) xs = Small xs
+isSmall :: Node -> Bool
+isSmall = all (isLower)
 
-isSmall :: Node a -> Bool
-isSmall (Small a) = True
-isSmall _         = False
-
-countPaths :: Node String -> Bool -> Graph -> Int
+countPaths :: Node -> Bool -> Graph -> Int
 countPaths n doublecaves = sum . countps n [] doublecaves
 
 -- Exclude large nums from the list. Eliminating any edges that result in going back to START should also prevent hat.
-countps :: Node String -> [Node String] -> Bool -> Graph -> [Int]
-countps END _ _ _ = [1]
+countps :: Node -> [Node] -> Bool -> Graph -> [Int]
+countps "end" _ _ _ = [1]
 countps n vst doubleUsed grph = concatMap (\e -> countps e (if isSmall n then n:vst else vst) (doubleUsed || e `elem` vst) grph) fromHere
         where fromHere 
-                | doubleUsed = filter (\x -> not (x `elem` vst)) $ fromJust $ lookup n grph -- Where can we get to from here?
-                | otherwise  = fromJust $ lookup n grph
+                | doubleUsed = filter (\x -> not (x `elem` vst)) $ fromJust $ HM.lookup n grph -- Where can we get to from here?
+                | otherwise  = fromJust $ HM.lookup n grph
 
 part1 :: String -> String
-part1 input = show . countPaths START True $ parse input
+part1 input = show . countPaths "start" True $ parse input
 
 part2 :: String -> String
-part2 input = show . countPaths START False $ parse input
+part2 input = show . countPaths "start" False $ parse input
