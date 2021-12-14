6626b3e678badfb1198a282b62611395eda8d058
Archie Hilton (thytom) <archie.hilton1@gmail.com>
2021-12-14 14:36:24 +0000

    Improve day 5

--- src/Days/Day5.hs
+++ src/Days/Day5.hs
@@ -10,8 +10,6 @@ import Data.HashTable.ST.Basic as H
 import Control.Monad.ST
 import Control.Monad
 
-import qualified Data.Map as M
-
 day = D.Day { D.name = "Day 5"
             , D.part1 = part1
             , D.part2 = part2
@@ -37,14 +35,19 @@ vertical [x1, _, x2, _] = x1 == x2
 horizontal :: Line -> Bool
 horizontal [_, y1, _, y2] = y1 == y2
 
-points :: Line -> [Point]
-points p@[x1, y1, x2, y2]
-  | vertical p   = zip [x1, x1..] [min y1 y2..max y1 y2]
-  | horizontal p = zip [min x1 x2..max x1 x2] [y1, y1..]
-  | otherwise = zip [x1, x1 + xcomp..x1 + xcomp * ((max x1 x2) - (min x1 x2))] 
-                    [y1, y1 + ycomp..y1 + ycomp * ((max y1 y2) - (min y1 y2))] 
+points :: [Point] -> [Line] -> [Point]
+points ps [] = ps 
+points ps (p@[x1, y1, x2, y2]:ls) 
+  | vertical p   = points (p1' (x1, min y1 y2) (x1, max y1 y2) (0, 1) ps) ls
+  | horizontal p = points (p1' (min x1 x2, y1) (max x1 x2, y1) (1, 0) ps) ls
+  | otherwise    = points (p1' (x1, y1) (x2, y2) (xcomp, ycomp) ps) ls
         where xcomp = (x2 - x1) `div` (abs (x2 - x1))
               ycomp = (y2 - y1) `div` (abs (y2 - y1))
+
+p1' :: Point -> Point -> (Int,Int) -> [Point] -> [Point]
+p1' a@(c_x, c_y) b@(t_x, t_y) (dx, dy) ps
+  | a == b = (a:ps)
+  | otherwise = p1' (c_x + dx, c_y + dy) b (dx, dy) (a:ps)
 
 intersections :: [Point] -> Int
 intersections ps = runST $ do ht1 <- newSized (length ps)
@@ -58,7 +61,8 @@ intersections ps = runST $ do ht1 <- newSized (length ps)
                               return sz
 
 part1 :: String -> String
-part1 = show . intersections . (>>=points) . filter (is_perpendicular) . parse
+part1 = show . intersections . points [] . filter (is_perpendicular) . parse
 
 part2 :: String -> String
-part2 = show . intersections . (>>=points) . parse
+part2 = show . intersections . points [] . parse
+
